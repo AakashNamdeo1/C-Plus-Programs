2753701d058b97e549ce2d29338928919f00aba7
AakashNamdeo1 <166501506+AakashNamdeo1@users.noreply.github.com>
2025-01-22 11:37:16 +0530

    Update Program 9.cpp

--- Program 9.cpp
+++ Program 9.cpp
@@ -1,12 +1,9 @@
-/*A book shop maintains the inventory of books that are being sold at the shop. The list includes details such 
-as author, title, price, publisher and stock position. Whenever a customer wants a book, the sales person 
-inputs the title and author and the system searches the list and displays whether it is available or not. If it is 
-not, an appropriate message is displayed. If it is, then the system displays the book details and requests for 
-the number of copies required. If the requested copies book details and requests for the number of copies 
-required. If the requested copies are available, the total cost of the requested copies is displayed; otherwise 
-the message “Required copies not in stock” is displayed. 
- Design a system using a class called books with suitable member functions and Constructors. Use new 
-operator in constructors to allocate memory space required.*/
+/* Improve the system design in exercise 8 to incorporate the following features:
+(a) The price of the books should be updated as and when required. Use a private meneber function to 
+implement this.
+(b) The stock value of each book should be automatically updated as soon as a transaction is completed.
+(c) The number of successful transactions should be recorded for the purpose of statistical analysis. Use 
+static data members to keep count of transaction.*/
 
 
 
