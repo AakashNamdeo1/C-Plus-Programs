0aa5f11a664b4c510fd8f7ca277d95669afd990b
AakashNamdeo1 <166501506+AakashNamdeo1@users.noreply.github.com>
2025-01-22 11:37:49 +0530

    Update Program 10.cpp

--- Program 10.cpp
+++ Program 10.cpp
@@ -1,3 +1,9 @@
+/* Design a C++ Class ‘Complex’ with data members for real and imaginary part. Provide default and 
+parameterized constructors. Write a program to perform arithmetic operations of two complex numbers 
+using operator overloading (using either member functions or friend functions).*/
+
+
+
 #include <iostream>
 
 class Complex {
