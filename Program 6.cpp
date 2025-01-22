efa955ac904e10a05a6cf4e7ee8fd8d4f054a0f4
AakashNamdeo1 <166501506+AakashNamdeo1@users.noreply.github.com>
2025-01-22 11:34:26 +0530

    Update Program 6.cpp

--- Program 6.cpp
+++ Program 6.cpp
@@ -1,3 +1,13 @@
+/*Create two classes DM and DB which store the value of distances. DM stores distances in meters and 
+centimeters and DB in feet and inches. Write a program that can read values for the class objects and odd 
+one object of DM with another object of DB.
+ Use a friend function to carry out the addition operation. The object that stores the results may be a 
+DM object or DB object, depending on the units in which the result are required. The display should be in 
+the format of feet and inches or meters and centimeters depending on the object on display.*/
+
+
+
+
 #include <iostream>
 
 class DB; // Forward declaration
